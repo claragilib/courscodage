37b9de3e686cdc59057c49796c9850f4c6f1f5c7
Clara <clara.gilibert>
2019-06-18 16:42:06 +0200

    DEFIS 1

---
+++ 2/script2.js
@@ -0,0 +1,13 @@
+
+
+$( document ).ready(function welcome() {
+	alert ("bienvenue")
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+
+    $ function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}
+});
